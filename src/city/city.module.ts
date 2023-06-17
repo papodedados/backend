d29ee21d99bae008d076f2de0dd764f949d14959
Leonardo <papodedados@gmail.com>
2023-06-17 19:41:22 -0300

    5_Tratamento_Exececao_User_Cidade

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -5,15 +5,10 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
 
-
 @Module({
-
-  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity]),
-    ],
-
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
   providers: [CityService],
-
+  exports: [CityService],
 })
-
 export class CityModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { hash } from 'bcrypt';
 import { Repository } from 'typeorm';
@@ -7,24 +7,38 @@ import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>,
-    ) {}
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
 
-    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
-        const saltOrRounds = 10;
-        const passwordHashed = await hash(createUserDto.password, saltOrRounds);
-        return this.userRepository.save({
-        ...createUserDto,
-        password: passwordHashed,
-        });
+    const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+    return this.userRepository.save({
+      ...createUserDto,
+      typeUser: 1,
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+
+  async findUserById(userId: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`UserId: ${userId} Not Found`);
     }
 
-    async getAllUser(): Promise<UserEntity[]> {
-        return this.userRepository.find();
-    }
-
-
+    return user;
+  }
 }
