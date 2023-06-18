1614bb9627bc3dea93b5559f6cadcdbf8467449a
Leonardo <papodedados@gmail.com>
2023-06-18 15:48:27 -0300

    8_JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,37 +6,50 @@ import { AddressModule } from './address/address.module';
 import { CityModule } from './city/city.module';
 import { StateModule } from './state/state.module';
 import { CacheModule } from './cache/cache.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local', '.env.development'],
+      envFilePath: ['.env.development.local'],
     }),
 
     TypeOrmModule.forRoot({
-      
-    host: 'localhost',
-    username: 'postgres',
-    password: 'docker',
-    database: 'newApp', 
-    type: 'postgres',
-    port: 5432,     
-    entities: [`${__dirname}/**/*.entity{.js,.ts}`],
-    migrations: [`${__dirname}/migration/{.ts,*.js}`],
-    migrationsRun: true,
+        
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      type: 'postgres',
+      port: Number(process.env.DB_PORT),     
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+
     
     //  synchronize: true, 
-    // database: process.env.DB_DATABASE,
-    // host:  process.env.DB_HOST,
-    // password:  'docker',
-    //   username:  process.env.DB_USERNAME, 
+    //  host:  process.env.DB_HOST,
+    //  username:  process.env.DB_USERNAME, 
+    //  password:  process.env.DB_PASSWORD,
+    //  type: process.env.DB_TYPE,
+    //  database: process.env.DB_DATABASE,
+
+
+    //  host: 'localhost',
+    //  username: 'postgres',
+    //  password: 'docker',
+    //  database: 'newApp', 
+    // v
+    //  port: 5432,     
     
     }), 
-    UserModule, AddressModule, CityModule, StateModule, CacheModule,
+    UserModule, AddressModule, CityModule, StateModule, CacheModule, AuthModule,
   ],
 
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+//console.log('teste  ' + process.env.DB_PASSWORD)
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/login.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<ReturnLogin> {
+    const user: UserEntity | undefined = await this.userService
+      .findUserByEmail(loginDto.email)
+      .catch(() => undefined);
+
+    const isMatch = await compare(loginDto.password, user?.password || '');
+
+    if (!user || !isMatch) {
+      throw new NotFoundException('Email or passord invalid');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
+  }
+}
