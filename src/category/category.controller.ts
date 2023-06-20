6fa506ae5737b90d836f460c4fd874d414406077
Leonardo <papodedados@gmail.com>
2023-06-20 18:07:43 -0300

    17_FUNÇÕES_PRODUTO

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,18 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { Roles } from 'src/decorators/roles.decorator';
-import { UserType } from 'src/user/enum/user-type.enum';
-import { CategoryService } from './category.service';
-import { ReturnCategory } from './dtos/return-category.dto';
-
-@Roles(UserType.Admin, UserType.User)
-@Controller('category')
-export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
-
-  @Get()
-  async findAllCategories(): Promise<ReturnCategory[]> {
-    return (await this.categoryService.findAllCategories()).map(
-      (category) => new ReturnCategory(category),
-    );
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    UsePipes,
+    ValidationPipe,
+  } from '@nestjs/common';
+  import { Roles } from 'src/decorators/roles.decorator';
+  import { UserType } from 'src/user/enum/user-type.enum';
+  import { CategoryService } from './category.service';
+  import { CreateCategory } from './dtos/create-category.dto';
+  import { ReturnCategory } from './dtos/return-category.dto';
+  import { CategoryEntity } from './entities/category.entity';
+  
+  @Roles(UserType.Admin, UserType.User)
+  @Controller('category')
+  export class CategoryController {
+    constructor(private readonly categoryService: CategoryService) {}
+  
+    @Get()
+    async findAllCategories(): Promise<ReturnCategory[]> {
+      return (await this.categoryService.findAllCategories()).map(
+        (category) => new ReturnCategory(category),
+      );
+    }
+  
+    @Roles(UserType.Admin)
+    @UsePipes(ValidationPipe)
+    @Post()
+    async createCategory(
+      @Body() createCategory: CreateCategory,
+    ): Promise<CategoryEntity> {
+      return this.categoryService.createCategory(createCategory);
+    }
   }
-}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,22 +1,57 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CategoryEntity } from './entities/category.entity';
-
-@Injectable()
-export class CategoryService {
-  constructor(
-    @InjectRepository(CategoryEntity)
-    private readonly categoryRepository: Repository<CategoryEntity>,
-  ) {}
-
-  async findAllCategories(): Promise<CategoryEntity[]> {
-    const categories = await this.categoryRepository.find();
-
-    if (!categories || categories.length === 0) {
-      throw new NotFoundException('Categories empty');
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+  } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import { Repository } from 'typeorm';
+  import { CreateCategory } from './dtos/create-category.dto';
+  import { CategoryEntity } from './entities/category.entity';
+  
+  @Injectable()
+  export class CategoryService {
+    constructor(
+      @InjectRepository(CategoryEntity)
+      private readonly categoryRepository: Repository<CategoryEntity>,
+    ) {}
+  
+    async findAllCategories(): Promise<CategoryEntity[]> {
+      const categories = await this.categoryRepository.find();
+  
+      if (!categories || categories.length === 0) {
+        throw new NotFoundException('Categories empty');
+      }
+  
+      return categories;
     }
-
-    return categories;
+  
+    async findCategoryByName(name: string): Promise<CategoryEntity> {
+      const category = await this.categoryRepository.findOne({
+        where: {
+          name,
+        },
+      });
+  
+      if (!category) {
+        throw new NotFoundException(`Category name ${name} not found`);
+      }
+  
+      return category;
+    }
+  
+    async createCategory(
+      createCategory: CreateCategory,
+    ): Promise<CategoryEntity> {
+      const category = await this.findCategoryByName(createCategory.name).catch(
+        () => undefined,
+      );
+  
+      if (category) {
+        throw new BadRequestException(
+          `Category name ${createCategory.name} exist`,
+        );
+      }
+  
+      return this.categoryRepository.save(createCategory);
+    }
   }
-}
